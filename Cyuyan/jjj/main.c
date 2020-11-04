42cec8698e8d167cb9c77d99a75d1a2a69b0a06b
Leeson0202 <asedrfa@163.com>
2020-11-04 23:07:31 +0800

    1104

--- Cyuyan/jjj/main.c
+++ Cyuyan/jjj/main.c
@@ -8,7 +8,7 @@ int main()
         a = num / 1000;   //万位
         b = num / 100 % 10;  //千位
         c = num % 100 / 10;  //十位
-        d = num % 1000;    //个位
+        d = num % 10;    //个位
         if (a == b && c == d && a != c)
         {
             printf("%d",num);
